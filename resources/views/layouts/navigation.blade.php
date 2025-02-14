722bf5ac7ee67f566f18a39a5ad04337b46ae26f
anacanestraro <ana.canestraro07@gmail.com>
2025-02-14 00:01:46 -0300

    add retiradas navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -41,6 +41,13 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('retirada.index')" :active="request()->routeIs('retirada.index')">
+                        {{ __('Retiradas') }}
+                    </x-nav-link>
+                </div>
+
+
             </div>
                 <!-- Settings Dropdown -->
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
