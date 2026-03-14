14faa476defc454e1283d2131def8db39b0a600e
anacanestraro <ana.canestraro07@gmail.com>
2026-03-14 14:41:09 -0300

    Closes #12: Implementação da seeder que roda todas juntas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'teste123'
+        $this->call([
+            UnidadeSeeder::class,
+            CategoriaSeeder::class,
+            EnderecoSeeder::class,
+            ClienteSeeder::class,
+            ProdutoSeeder::class,
         ]);
     }
 }
