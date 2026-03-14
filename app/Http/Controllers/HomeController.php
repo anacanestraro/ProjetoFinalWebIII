34d1dc6403805206711795681fe26636fc85a5d9
anacanestraro <ana.canestraro07@gmail.com>
2026-03-13 21:33:01 -0300

    Related #02: Implementação do controller da homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
+use App\Models\Cliente;
+use App\Models\Retirada;
+use App\Models\Categoria;
 
 class HomeController extends Controller
 {
     public function home(){
-        return view('home');
+        $totalProdutos      = Produto::count();
+        $totalClientes      = Cliente::count();
+        $totalRetiradas     = Retirada::count();
+        $totalCategorias    = Categoria::count();
+        $semEstoque         = Produto::where('estoque', 0)->orWhereNull('estoque')->count();
+        $ultimasRetiradas   = Retirada::with(['cliente', 'produtos'])->latest()->take(5)->get();
+        $produtosCriticos   = Produto::where('estoque', '<=', 5)->orderBy('estoque')->take(5)->get();
+
+        return view('home', compact(
+            'totalProdutos',
+            'totalClientes',
+            'totalRetiradas',
+            'totalCategorias',
+            'semEstoque',
+            'ultimasRetiradas',
+            'produtosCriticos'
+        ));
     }
 }
