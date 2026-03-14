f158dde58d642f44d7ac8dd52343c68a46b5ded3
anacanestraro <ana.canestraro07@gmail.com>
2026-03-14 14:45:11 -0300

    Related #11: Implmentando a lógica de paginação em categoria

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -8,11 +8,7 @@ use App\Models\Categoria;
 class CategoriaController extends Controller
 {
     public function index(){
-        $categorias = Categoria::all();
-
-        if($categorias->isEmpty()){
-            session()->flash('mensagem', 'Nenhuma categoria cadastrada.');
-        }
+        $categorias = Categoria::paginate(15);
         return view('categoria.index', compact('categorias'));
     }
 
@@ -22,20 +18,12 @@ class CategoriaController extends Controller
 
     public function store(Request $request){
         $validated = $request->validate([
-            'nome' => 'required',
-            'descricao'=>'required'
+            'nome'     => 'required',
+            'descricao'=> 'required',
         ]);
 
-        $status = Categoria::create([
-            'nome' => $validated['nome'],
-            'descricao' => $validated['descricao']
-        ]);
-
-        if($status){
-            return redirect()->route('categoria.index')->with('mensagem', 'Categoria cadastrada com sucesso!');
-        }else{
-            return back()->with('mensagem', 'Erro ao cadastrar categoria. Tente novamente.');
-        }
+        Categoria::create($validated);
+        return redirect()->route('categoria.index')->with('mensagem', 'Categoria cadastrada com sucesso!');
     }
 
     public function show(Categoria $categoria){
@@ -43,35 +31,25 @@ class CategoriaController extends Controller
     }
 
     public function edit(Categoria $categoria){
-        return view ('categoria.edit', compact('categoria'));
+        return view('categoria.edit', compact('categoria'));
     }
-    
+
     public function update(Categoria $categoria, Request $request){
         $request->validate([
-            'nome' => 'required',
-            'descricao'=>'required'
+            'nome'     => 'required',
+            'descricao'=> 'required',
         ]);
 
-        $status = $categoria->update([
-            'nome' => $request->nome,
-            'descricao' => $request->descricao
+        $categoria->update([
+            'nome'     => $request->nome,
+            'descricao'=> $request->descricao,
         ]);
 
-        if($status){
-            return redirect()->route('categoria.index')->with('mensagem', 'Categoria atualizada com sucesso!');
-        }else{
-            return back()->with('mensagem', 'Erro ao atualizar categoria. Tente novamente.');
-        }
+        return redirect()->route('categoria.index')->with('mensagem', 'Categoria atualizada com sucesso!');
     }
 
     public function destroy(Categoria $categoria){
-        $status = $categoria->delete();
-
-        if($status){
-            return redirect()->route('categoria.index')->with('mensagem', 'Categoria deletada com sucesso!');
-            
-        }else{
-            return back()->with('mensagem', 'Erro ao deletar categoria. Tente novamente.');
-        }
+        $categoria->delete();
+        return redirect()->route('categoria.index')->with('mensagem', 'Categoria deletada com sucesso!');
     }
 }
