0d56093f84005969ec36ae2df4b0a96d14b2ab73
anacanestraro <ana.canestraro07@gmail.com>
2026-03-15 15:15:37 -0300

    Related #13: Implementação do model de movimentação

---
+++ app/Models/Movimentacao.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movimentacao extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'produto_id',
+        'user_id',
+        'tipo',
+        'quantidade',
+        'estoque_antes',
+        'estoque_depois',
+        'motivo',
+    ];
+
+    public function produto()
+    {
+        return $this->belongsTo(Produto::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
